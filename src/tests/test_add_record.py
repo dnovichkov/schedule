b2ee92d87a4ad7b183386d7a834c7b614420b835
novichkov <novichkov@smartsolutions-123.ru>
2020-03-03 09:55:28 +0400

    Add testcase stub for schedule with range

--- src/tests/test_add_record.py
+++ src/tests/test_add_record.py
@@ -1,13 +1,47 @@
 import datetime
+import logging
 
 import pytest
 from datetimerange import DateTimeRange
 from schedule.schedule import Schedule
 
 
-def test_simple_add():
+def test_simple_add_to_schedule_duration():
     start_datetime = datetime.datetime(year=2020, month=1, day=1)
     schedule = Schedule(start_datetime, duration=3600)
+
+    _range = DateTimeRange(
+        start_datetime, start_datetime + datetime.timedelta(minutes=1))
+    schedule.add_record('Test_type', _range)
+    assert len(schedule.records) == 1
+
+    _range = DateTimeRange(
+        start_datetime + datetime.timedelta(minutes=1), start_datetime + datetime.timedelta(minutes=2))
+    schedule.add_record('Test_type', _range)
+    assert len(schedule.records) == 2
+
+
+def test_simple_add_to_schedule_range():
+    start_datetime = datetime.datetime(year=2020, month=1, day=1)
+    end_datetime = start_datetime + datetime.timedelta(hours=1)
+    schedule = Schedule(start_datetime, duration=7200, end_dt=end_datetime)
+
+    _range = DateTimeRange(
+        start_datetime, start_datetime + datetime.timedelta(minutes=1))
+    schedule.add_record('Test_type', _range)
+    assert len(schedule.records) == 1
+
+    _range = DateTimeRange(
+        start_datetime + datetime.timedelta(minutes=1), start_datetime + datetime.timedelta(minutes=2))
+    schedule.add_record('Test_type', _range)
+    assert len(schedule.records) == 2
+
+
+def test_simple_add_to_schedule_duration_and_range(caplog):
+    caplog.set_level(logging.INFO)
+    start_datetime = datetime.datetime(year=2020, month=1, day=1)
+    end_datetime = start_datetime + datetime.timedelta(hours=1)
+    schedule = Schedule(start_datetime, end_dt=end_datetime)
 
     _range = DateTimeRange(
         start_datetime, start_datetime + datetime.timedelta(minutes=1))
